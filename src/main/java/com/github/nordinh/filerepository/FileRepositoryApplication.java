37537e83c2b22bcebc19503879f5bd9302450645
nordinh <nordin.haouari@gmail.com>
2015-02-06 13:29:37 +0100

    Added swagger documentation @ build time + serving as asset

--- src/main/java/com/github/nordinh/filerepository/FileRepositoryApplication.java
+++ src/main/java/com/github/nordinh/filerepository/FileRepositoryApplication.java
@@ -1,8 +1,17 @@
 package com.github.nordinh.filerepository;
 
 import io.dropwizard.Application;
+import io.dropwizard.assets.AssetsBundle;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+
+import java.net.UnknownHostException;
+import java.util.EnumSet;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+
+import org.eclipse.jetty.servlets.CrossOriginFilter;
 
 import com.commercehub.dropwizard.mongo.ManagedMongoClient;
 import com.github.nordinh.filerepository.health.MongoHealthCheck;
@@ -22,15 +31,36 @@ public class FileRepositoryApplication extends Application<FileRepositoryConfigu
 
 	@Override
 	public void initialize(Bootstrap<FileRepositoryConfiguration> bootstrap) {
+		bootstrap.addBundle(new AssetsBundle("/api-doc"));
 	}
 
 	@Override
 	public void run(FileRepositoryConfiguration configuration, Environment environment) throws Exception {
+		DB db = configureMongoDB(configuration, environment);
+		configureCORS(environment);
+		environment.jersey().register(new FileResource(db));
+	}
+
+	protected DB configureMongoDB(FileRepositoryConfiguration configuration, Environment environment)
+			throws UnknownHostException {
 		ManagedMongoClient mongoClient = configuration.getMongo().build();
 		environment.lifecycle().manage(mongoClient);
 		DB db = mongoClient.getDB(configuration.getMongo().getDbName());
 		environment.healthChecks().register("mongoDB", new MongoHealthCheck(db));
-		environment.jersey().register(new FileResource(db));
+		return db;
+	}
+
+	protected void configureCORS(Environment environment) {
+		final FilterRegistration.Dynamic cors =
+				environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+
+		// Configure CORS parameters
+		cors.setInitParameter("allowedOrigins", "*");
+		cors.setInitParameter("allowedHeaders", "X-Requested-With,Content-Type,Accept,Origin");
+		cors.setInitParameter("allowedMethods", "OPTIONS,GET,PUT,POST,DELETE,HEAD");
+
+		// Add URL mapping
+		cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
 	}
 
 }
--- src/main/java/com/github/nordinh/filerepository/resources/FileResource.java
+++ src/main/java/com/github/nordinh/filerepository/resources/FileResource.java
@@ -27,8 +27,16 @@ import com.mongodb.gridfs.GridFSDBFile;
 import com.mongodb.gridfs.GridFSInputFile;
 import com.sun.jersey.core.header.FormDataContentDisposition;
 import com.sun.jersey.multipart.FormDataParam;
+import com.wordnik.swagger.annotations.Api;
+import com.wordnik.swagger.annotations.ApiOperation;
+import com.wordnik.swagger.annotations.ApiParam;
+import com.wordnik.swagger.annotations.ApiResponse;
+import com.wordnik.swagger.annotations.ApiResponses;
 
 @Path("/{bucket}")
+@Api(
+		value = "File Store",
+		description = "File operations for a particular bucket")
 public class FileResource {
 
 	private DB db;
@@ -40,9 +48,17 @@ public class FileResource {
 	@POST
 	@Consumes(MediaType.MULTIPART_FORM_DATA)
 	@Timed
+	@ApiOperation(
+			value = "Store a file in the repository",
+			notes = "Stores a file sent using multipart form data into the bucket. "
+					+ "Files are identified by their file name. "
+					+ "If a file with the same name already exists it will get replaced.")
+	@ApiResponses(value = {
+			@ApiResponse(code = 201, message = "The file has been stored and is accessible on the given location")
+	})
 	public Response storeFile(
-			@PathParam("bucket") String bucket,
-			@FormDataParam("file") InputStream fileInputStream,
+			@PathParam("bucket") @ApiParam(value = "The bucket in which to store the file", required = true) String bucket,
+			@FormDataParam("file") @ApiParam(value = "The (binary) file to be stored", required = true) InputStream fileInputStream,
 			@FormDataParam("file") FormDataContentDisposition contentDispositionHeader) {
 
 		GridFS gridFS = new GridFS(db, bucket);
@@ -62,9 +78,16 @@ public class FileResource {
 	@Path("{fileName}")
 	@Produces(MediaType.APPLICATION_OCTET_STREAM)
 	@Timed
+	@ApiOperation(
+			value = "Read a file from the repository",
+			notes = "Reads the file identified by the given file name from the bucket. Returns a binary stream.")
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "File was found"),
+			@ApiResponse(code = 500, message = "File was not found")
+	})
 	public Response readFile(
-			@PathParam("bucket") String bucket,
-			@PathParam("fileName") String fileName) {
+			@PathParam("bucket") @ApiParam(value = "The bucket from which to read the file", required = true) String bucket,
+			@PathParam("fileName") @ApiParam(value = "The file name of the file to be retrieved", required = true) String fileName) {
 
 		GridFS gridFS = new GridFS(db, bucket);
 		GridFSDBFile gridFSDBFile = gridFS.findOne(decode(fileName));
@@ -87,9 +110,15 @@ public class FileResource {
 	@DELETE
 	@Path("{fileName}")
 	@Timed
+	@ApiOperation(
+			value = "Delete a file from the repository",
+			notes = "Deletes the file identified by the given file name from the bucket. If the file does not exist nothing happens.")
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "File deletion was successfull")
+	})
 	public void deleteFile(
-			@PathParam("bucket") String bucket,
-			@PathParam("fileName") String fileName) {
+			@PathParam("bucket") @ApiParam(value = "The bucket from which to delete the file", required = true) String bucket,
+			@PathParam("fileName") @ApiParam(value = "The file name of the file to be deleted", required = true) String fileName) {
 
 		GridFS gridFS = new GridFS(db, bucket);
 		gridFS.remove(decode(fileName));
